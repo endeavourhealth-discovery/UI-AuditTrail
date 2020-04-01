2d13999459f9fd4bd247a9956c470f029257b225
stuart <stuart@bearclawsolutions.co.uk>
2020-04-01 15:43:14 +0100

    Use ConnectionManager to get a connection

--- API/src/main/java/org/endeavourhealth/uiaudit/dal/ConnectionPool.java
+++ API/src/main/java/org/endeavourhealth/uiaudit/dal/ConnectionPool.java
@@ -1,16 +1,14 @@
 package org.endeavourhealth.uiaudit.dal;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import org.endeavourhealth.common.cache.GenericCache;
 import org.endeavourhealth.common.config.ConfigManager;
 import org.endeavourhealth.common.utility.MetricsHelper;
+import org.endeavourhealth.core.database.rdbms.ConnectionManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 public class ConnectionPool extends GenericCache<Connection> {
     private static final Logger LOG = LoggerFactory.getLogger(ConnectionPool.class);
@@ -48,53 +46,18 @@ public class ConnectionPool extends GenericCache<Connection> {
     }
 
     @Override
-    protected Connection create() {
+    protected Connection create()  {
+
         try {
+            String appId = ConfigManager.getAppId();
 
-            String url;
-            String user;
-            String pass;
-            String driver;
-            JsonNode json = ConfigManager.getConfigurationAsJson("database");
+            switch (appId) {
+                case "data-sharing-manager"  :   return ConnectionManager.getDataSharingManagerConnection();
+                case "user-manager"          :   return ConnectionManager.getUserManagerConnection();
 
-            if (json != null) {
-                url = json.get("url").asText();
-                user = json.get("username").asText();
-                pass = json.get("password").asText();
-                driver = json.get("class") == null ? null : json.get("class").asText();
-            } else {
-                // get new style of config
-
-                String appId = ConfigManager.getAppId();
-
-                String dbConnectionConfigItem = "db_" + appId.replace("-", "_");
-
-                json = ConfigManager.getConfigurationAsJson(dbConnectionConfigItem, "global");
-
-                url = json.get("url").asText();
-                driver = json.get("class") == null ? null : json.get("class").asText();
-
-                json = ConfigManager.getConfigurationAsJson("db_credentials");
-
-                user = json.get("username").asText();
-                pass = json.get("password").asText();
-
+                default: throw new IllegalArgumentException("appId not recognised : " + appId);
             }
 
-            if (driver != null && !driver.isEmpty())
-                Class.forName(driver);
-
-            Properties props = new Properties();
-
-            props.setProperty("user", user);
-            props.setProperty("password", pass);
-
-            Connection connection = DriverManager.getConnection(url, props);    // NOSONAR
-
-            LOG.debug("New DB Connection created");
-
-            MetricsHelper.recordCounter("ConnectionPool.Size").inc();
-            return connection;
         } catch (Exception e) {
             LOG.error("Error getting connection", e);
         }
